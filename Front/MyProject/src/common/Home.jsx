ef5f29eabdc6482e75164328c91de814ef0dfdd2
Dmytro <bardmyt@gmail.com>
2023-08-18 18:18:42 +0300

    adjusted home

--- Front/MyProject/src/common/Home.jsx
+++ Front/MyProject/src/common/Home.jsx
@@ -23,15 +23,15 @@ export default function Home(){
         <div className="home-container">
             <h2 className="home-heading">Welcome to Pet 2 Pet Community!</h2>
             <p className="home-text">We've positively impacted the lives of over 1 million individuals through
-             our innovative platform, where caring hearts come together to provide mutual pet care and support. </p>
-            <p className="home-heading">About us</p>
-            <p className="home-text">At Pet 2 Pet, we believe in the power of a helping
+             our innovative platform, where caring hearts come together to provide mutual pet care and support.
+             At Pet 2 Pet, we believe in the power of a helping
              hand and a caring heart. Our platform is designed to bring people closer
               by fostering trust, empathy, and a sense of responsibility.
                Whether you're a devoted pet owner seeking a reliable companion for your furry
-                friend or someone eager to lend a hand and shower another pet with affection, you're in the right place.</p>
+                friend or someone eager to lend a hand and shower another pet with affection, you're in the right place.
+              </p>
              <hr className="home-separator"></hr>
-              <h3 className="home-heading">Our pets</h3>
+              <p className="home-subheading">Our pets</p>
               
               {pets.map(pet => {
                 return <PetInfo key={pet.id} item={pet} />;
